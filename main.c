844bfe12bd0227fd20fa0dedca802de28d39219a
goz <goran.sikoski@Gmail.com>
2025-01-24 11:55:24 +1100

    Added function base

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "gameStructs.h"
+#include "gamelib.h"
 
 player initialiseCharacter() {
     
     player* c_ptr = malloc(sizeof(player));
 
-    printf("Hello player, welcome to the dungeon!\n");
-    printf("What is your name?\n");
+    scrollPrint("Hello player, welcome to the dungeon!\n", 60);
+    scrollPrint("What is your name?\n", 60);
     scanf("%s", c_ptr->name);
+    printf("%c", '\n');
     c_ptr->health = 100;
 
     player c = *c_ptr;
@@ -19,12 +20,12 @@ player initialiseCharacter() {
 }
 
 int startGame() {
-    printf("You wake up in a damp cave\n");
-    printf("A dim light is glowing in the distance\n");
-    printf("You can't make out your surroundings so you walk towards the light\n");
-    printf("A terrible stench attacks your senses as you walk towards entrance\n");
-    printf("It is almost unbearable\n");
-    printf("As you approach the light you notice a bruises on your arms\n");
+    scrollPrint("You wake up in a damp cave\n", 60);
+    scrollPrint("A dim light is glowing in the distance\n", 60);
+    scrollPrint("You can't make out your surroundings so you walk towards the light\n", 60);
+    scrollPrint("A terrible stench attacks your senses as you walk towards entrance\n", 60);
+    scrollPrint("It is almost unbearable\n", 60);
+    scrollPrint("As you approach the light you notice bruises on your arms\n", 60);
     return 0;
 }
 
